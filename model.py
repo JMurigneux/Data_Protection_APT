2bfccca4222f385332d134b7a23731ed966afe87
BOYER <mathieu2000.b@gmail.com>
2022-11-23 16:22:14 +0100

    maj model

--- model.py
+++ model.py
@@ -17,62 +17,74 @@ from sklearn.ensemble import GradientBoostingClassifier
 
 from xgboost import XGBClassifier,plot_importance,plot_tree
 
-class cmodel():
-    le = preprocessing.LabelEncoder()
+import tracemalloc
 
-    models = {
-        "XGBoost" : XGBClassifier(),
-        "KNN" : KNeighborsClassifier(),
-        "CART":DecisionTreeClassifier(),
-        "Random Forrest" : RandomForestClassifier(),
-        "SVM" : SVC(),
-        "MLP" : MLPClassifier(),
-    }
+label_encoder= preprocessing.LabelEncoder()
+
+models = {
+    "XGBoost" : XGBClassifier(),
+    "KNN" : KNeighborsClassifier(),
+    "CART":DecisionTreeClassifier(),
+    "Random Forrest" : RandomForestClassifier(),
+    "SVM" : SVC(),
+    "MLP" : MLPClassifier(),
+}
 
 
-    def pretraitment(self, dataset):
-        X = dataset.copy()
-        Y = dataset["Label"]
-        Y.to_frame()
-        X = X.drop(columns = ['Label'])
-        col = list(dataset.select_dtypes(include=['O']))
-        col = col[:-1]
-        enc = OneHotEncoder(handle_unknown='ignore')
-        transformed = pd.DataFrame(enc.fit_transform(X[col]).toarray())
-        X = X.join(transformed)
-        X = X.drop(col,axis=1)
-        return X,Y
+def pretraitment(dataset):
+    X = dataset.copy()
+    Y = dataset["Label"]
+    Y.to_frame()
+    X = X.drop(columns = ['Label'])
+    col = list(dataset.select_dtypes(include=['O']))
+    col = col[:-1]
+    enc = OneHotEncoder(handle_unknown='ignore')
+    transformed = pd.DataFrame(enc.fit_transform(X[col]).toarray())
+    X = X.join(transformed)
+    X = X.drop(col,axis=1)
+    return X,Y
 
-    def init_model(self, model_name, train_and_test_dataset):
-        X,Y = self.pretraitment(train_and_test_dataset)
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, Y,train_size=0.3, shuffle = True)
-        
-        y_train_transformed = self.le.fit_transform(self.y_train)
+def evaluation(model,X_test,y_test):
+    y_predictions_transformed = model.predict(X_test)
 
-        self.models[model_name].fit(self.X_train, y_train_transformed)
+    y_test_t = label_encoder.transform(y_test)
+    conf_matrix = confusion_matrix(y_test_t, y_predictions_transformed)
+    sn.heatmap(conf_matrix)
 
-        return self.models[model_name]
+    print("The precision, the recall, and the accuracy can be found in the following classification report :")
+    print(classification_report(y_test_t, y_predictions_transformed))
 
-    def evaluation(self,model_name):
-        y_predictions_transformed = self.models[model_name].predict(self.X_test)
+    print("The balanced accuracy : " + str(balanced_accuracy_score(y_test_t, y_predictions_transformed)))
+    print("The Matthews Correlation Coefficient : " + str(matthews_corrcoef(y_test_t,y_predictions_transformed)))
+    return
 
-        y_test_t = self.le.transform(self.y_test)
-        conf_matrix = confusion_matrix(y_test_t, y_predictions_transformed)
-        sn.heatmap(conf_matrix)
+def init_model(model_name, train_and_test_dataset, with_evaluation = True):
+    X,Y = pretraitment(train_and_test_dataset)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y,train_size=0.3, shuffle = True)
+    
+    y_train_transformed = label_encoder.fit_transform(y_train)
 
-        print("The precision, the recall, and the accuracy can be found in the following classification report :")
-        print(classification_report(y_test_t, y_predictions_transformed))
+    model = models[model_name]
+    model.fit(X_train, y_train_transformed)
 
-        print("The balanced accuracy : " + str(balanced_accuracy_score(y_test_t, y_predictions_transformed)))
-        print("The Matthews Correlation Coefficient : " + str(matthews_corrcoef(y_test_t,y_predictions_transformed)))
-        return
+    if with_evaluation:
+        evaluation(model, X_test=X_test, y_test=y_test)
 
-    def is_malware(self, model, malware_features):
-        Boolean,probability = False, 0
-        return (Boolean, probability)
+    return model
+
+def is_malware(self, model, malware_features):
+    Boolean,probability = False, 0
+    return (Boolean, probability)
+
+def consumption_mesure(model_name, train_and_test_dataset):
+
+    tracemalloc.start()
+    model = init_model(model_name, train_and_test_dataset, evaluation=False)
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+    tracemalloc.stop()
+    return
 
 
 df = pd.read_csv("dataset/MSCAD.csv")
-xgb_model = cmodel()
-xgb_model.init_model("XGBoost",df)
-xgb_model.evaluation("XGBoost")
+xgb_model = init_model("XGBoost",df)
